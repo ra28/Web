e0ea6f49a52b275fd6182f07633ded0d2b614bf3
Mihailo <lukicmihailo@gmail.com>
2019-01-30 21:05:14 +0100

    first test service

---
+++ SchoolApplication/SchoolApp.Server/Models/BookServiceContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SchoolApp.Server.Models
+{
+    public class BookServiceContext : DbContext
+    {
+        public BookServiceContext() : base("name=BookServiceContext")
+        {
+            this.Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
+        }
+
+        public System.Data.Entity.DbSet<SchoolApp.Server.Models.Author> Authors { get; set; }
+
+        public System.Data.Entity.DbSet<SchoolApp.Server.Models.Book> Books { get; set; }
+
+    }
+}
